2995ffdb500c993df2b53b07a02e8b9dc5d8ddce
Keshav Jha <keshavjha0610@gmail.com>
2023-05-06 11:48:40 +0530

    modified 02_arrays.js

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,17 +1,17 @@
 const marvel_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
 
-// marvel_heros.push(dc_heros)
+// marvel_heros.push(dc_heros) - push merges one array to another
 
 // console.log(marvel_heros);
-// console.log(marvel_heros[3][1]);
+// console.log(marvel_heros[3][1]); flash
 
 // const allHeros = marvel_heros.concat(dc_heros)
-// console.log(allHeros);
+// console.log(allHeros); concat combines two arrays and give one combined array
 
 const all_new_heros = [...marvel_heros, ...dc_heros]
 
-// console.log(all_new_heros);
+// console.log(all_new_heros); spread operator uses ...
 
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
@@ -19,13 +19,12 @@ const real_another_array = another_array.flat(Infinity)
 console.log(real_another_array);
 
 
-
-console.log(Array.isArray("Keshav"))
-console.log(Array.from("Keshav"))
-console.log(Array.from({ name: "keshav" })) // interesting
+console.log(Array.isArray("Keshav")) //false
+console.log(Array.from("Keshav")) //makes array ['K' , 'e', 's', 'h', 'a', 'v']
+console.log(Array.from({ name: "keshav" })) // interesting - gives empty array (need to give keys or values to form array)
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+console.log(Array.of(score1, score2, score3)); // [100 , 200 , 300]

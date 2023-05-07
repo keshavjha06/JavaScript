950ee2185f854a1bbf6f4d025daca462799830ca
Keshav Jha <keshavjha0610@gmail.com>
2023-05-07 19:52:05 +0530

    Added project 1

---
+++ proj1/script.js
@@ -0,0 +1,13 @@
+let counter = document.querySelector(".counter")
+let followers = document.querySelector(".followers")
+let count = 1;
+
+setInterval(() => {
+    if (count < 1000)
+        count++;
+    counter.innerText = count;
+}, 1)
+
+setTimeout(() => {
+    followers.innerText = "Followers on Instagram!"
+}, 5000)

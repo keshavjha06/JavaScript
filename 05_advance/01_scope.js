922a267ec9d2fc6fa40ad99e7092120627c52f17
Keshav Jha <keshavjha0610@gmail.com>
2023-05-10 10:38:17 +0530

    Added scope.js

---
+++ 05_advance/01_scope.js
@@ -0,0 +1,10 @@
+// console.log(name);
+
+var name = "Keshav";
+
+if (true) {
+    let lastName = "Jha";
+}
+console.log(lastName);
+
+//curly braces {} are scopes.In Js scopes are functions and not loops

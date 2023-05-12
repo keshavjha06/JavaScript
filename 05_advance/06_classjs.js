612c5a5063b5b5b05bd175e0ee174cd1a2cf974d
Keshav Jha <keshavjha0610@gmail.com>
2023-05-12 20:35:49 +0530

    added keywords in classjs.js

--- 05_advance/06_classjs.js
+++ 05_advance/06_classjs.js
@@ -13,6 +13,22 @@ class User {
     getCourseList() {
         return this.#courseList; // getter , return the variable and no params usually 
     }
+
+    static login() {    // static methods cannot be inherited
+        return "you are logged in"
+    }
+}
+
+class SubAdmin extends User {
+    constructor(name, email) {
+        super(name, email) // super keyword can be used to call method/constructor from parent class
+    }
+    getAdminInfo() {
+        return "I am subadmin"
+    }
+    login() { //if the method name is same in parent & child , then child method will be called
+        return "login for admin only"
+    }
 }
 
 module.exports = User;
@@ -22,3 +38,10 @@ console.log(rock.getCourseList());
 rock.enrollCourse("Angular Bootcamp")
 console.log(rock.getCourseList());
 console.log(rock.courseList);
+
+
+const tom = new SubAdmin("tom", "tom@jerry.com")
+console.log(tom.getAdminInfo());
+console.log(tom.login());
+
+console.log(tom.getInfo());

843bb8732f2150f225ab574842d2a23d9a46e9ad
Keshav Jha <keshavjha0610@gmail.com>
2023-05-17 20:23:38 +0530

    modified truthy_and_falsy

--- 01_basics/12_truthy_and_falsy.js
+++ 01_basics/12_truthy_and_falsy.js
@@ -19,3 +19,18 @@ if (2 === user) {
 }
 
 // console.log("2" + "2");
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10 // 5
+// val1 = null ?? 10 // 10
+val1 = undefined ?? 15 // 15
+val1 = undefined ?? 10 ?? 15 // 10
+console.log(val1);
+
+// Ternary operator:
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
